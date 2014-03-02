0d9c946d75d74584598857bb07187ec6c7124453
MrKireko <mrkireko@gmail.com>
2014-03-02 14:43:51 +0100

    Redone Everything

---
+++ js.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+    
+    $("#link1").click(function() {
+        document.location = "http://www.twitter.com/OnlineLoM";
+    });
+    
+    $("#link2").click(function() {
+        document.location = "http://www.twitter.com/MrKireko";
+    });
+    
+    $("#link3").click(function() {
+        document.location = "http://www.twitter.com/XozaLo";
+    });
+    
+});
