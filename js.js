2a4bebb26a2ce4ccb64cab387bc70aa7072fd7f2
MrKireko <mrkireko@gmail.com>
2014-03-02 21:13:39 +0100

    Added YouTube Link

--- js.js
+++ js.js
@@ -3,13 +3,13 @@ $(document).ready(function() {
     $("#link1").click(function() {
         document.location = "http://www.twitter.com/OnlineLoM";
     });
-    
     $("#link2").click(function() {
         document.location = "http://www.twitter.com/MrKireko";
     });
-    
     $("#link3").click(function() {
         document.location = "http://www.twitter.com/XozaLo";
     });
-    
+    $("#link4").click(function() {
+        document.location = "http://www.youtube.com/OfficialLoM1";
+    });
 });
